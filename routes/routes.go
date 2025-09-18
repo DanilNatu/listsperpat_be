cd795bd44c0794856366b1a6f6164962623836ed
Denilson <natudenilson@gmail.com>
2025-09-19 02:56:00 +0800

    feat: add CORS support for API routes

--- routes/routes.go
+++ routes/routes.go
@@ -3,11 +3,23 @@ package routes
 import (
 	"listSparepart/controllers"
 
+	"time"
+
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func Routes() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://localhost:8005"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	r.GET("/sparepart", controllers.GetAllSparepart)
 	r.POST("/sparepart", controllers.CreateSparepart)
