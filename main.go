0086ff03ab9a0dc82cb5990bf80babd27595c412
Denilson <natudenilson@gmail.com>
2025-09-18 23:30:58 +0800

    chore: setup golang project listsperpart with database connection

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+	config "listsperpat/db"
+	"listsperpat/routes"
+)
+
+func main() {
+	config.ConnectDatabase()
+	r := routes.Routes()
+	fmt.Println("ListSperpart API berjalan di http://localhost:8080")
+	r.Run(":8080")
+}
